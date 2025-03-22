1ae319f51203d93c13f639ced880f45451868fd5
Jeta Krasniqi <jeta.krasniqi7@student.uni-pr.edu>
2025-03-22 11:08:23 +0100

    Update My_bankaccount.java
    Test class for BankAccount class witch takes input from the user to update the balance

--- My_bankaccount.java
+++ My_bankaccount.java
@@ -7,18 +7,18 @@ public class My_bankaccount {
 		my_account.deposit(150);
 		my_account.withdraw(200);
 		my_account.getBalance();
-		char s = my_account.readCommand("Type D (for deposit) and W (for withdraw)");
-		int a = my_account.readCommand("Type the amount: ");
+		char s = my_account.readCommand("Type D (for deposit) and W (for withdraw) followed by the amount: ");
+		int amount = my_account.readCommand();
 		if (s == 'W') {
-			my_account.withdraw(a);
+			my_account.withdraw(amount);
 		}
 		else if (s == 'D') {
-			my_account.deposit(a);
+			my_account.deposit(amount);
 		}
 		else {
 			 System.out.println("Invalid input");
 		}
-        System.out.println("You have " + my_account.getBalance() + " dollars in your Bank Account");
+        System.out.println("You have " + (my_account.getBalance()/(100.0)) + " dollars in your Bank Account");
 	}
 
 }
